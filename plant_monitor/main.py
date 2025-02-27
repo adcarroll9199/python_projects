0cf44c37efd5498947b606638e0e0fae1e866a05
Adam <adcarroll9199@gmail.com>
2025-02-27 08:23:18 -0500

    Leverage Util Library for response message

--- plant_monitor/main.py
+++ plant_monitor/main.py
@@ -3,18 +3,21 @@
 ## ^^ For Unix compatability, I think it is ignored by Windows
 
 from micropyserver import MicroPyServer
+import utils
 
 server = MicroPyServer(host="0.0.0.0", port=8000)
 content =''
 indexFile = 'index.html'
 
 def index(request):
+    contentLength = f"Content-Length: {str(len(content.encode()))}"
+    headers = [contentLength, "Connection: close"]
+    utils.send_response(server, content, extend_headers=headers)
     print('<--- Received Request')
-    contentLength = len(content.encode())
-    response = f'HTTP/1.0 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: {contentLength}\r\nConnection: close\r\n'
-    response += "\r\n"
-    response += content
-    server.send(response)
+#    response = f'HTTP/1.0 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: {contentLength}\r\nConnection: close\r\n'
+#    response += "\r\n"
+#    response += content
+#    server.send(response)
     print('---> Response Sent')
 
 
